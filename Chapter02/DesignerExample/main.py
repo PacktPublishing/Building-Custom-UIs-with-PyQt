50b7ff552791861409977b55d8133a0f764187e7
Joshua Willman <joshua.willman@ymail.com>
2021-06-24 15:53:00 +0800

    Add files via upload

--- Chapter02/DesignerExample/main.py
+++ Chapter02/DesignerExample/main.py
@@ -1,1 +1,17 @@
+"""Qt Designer Example GUI
+Question 5 – Demonstrates how to use Qt Designer 
+and XML .ui files in PyQt6
+
+Building Custom UIs with PyQt with Packt Publishing
+Chapter 2 - Building the Foundation for GUIs
+Created by: Joshua Willman
+"""
+
 import sys
+from PyQt6.QtWidgets import QApplication
+from designer_example.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
