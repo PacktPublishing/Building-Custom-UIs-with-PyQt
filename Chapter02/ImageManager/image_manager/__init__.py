37c140cda9bb5c2823ff72ec7aff083e055235f5
joshuawillman <joshua.willman@ymail.com>
2022-08-02 00:47:32 -0700

    commit changes

--- Chapter02/ImageManager/image_manager/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# image_manager/__init__.py
-# Import necessary modules
-from PyQt6.QtWidgets import QApplication
-
-"""This module sets up the image_manager package."""
-QApplication.setApplicationName("Image Manager")
-__version__ = QApplication.setApplicationVersion('1.1.0')
--- Chapter03/BudgetTracker/budget_tracker/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# image_manager/__init__.py
-# Import necessary modules
-from PyQt6.QtWidgets import QApplication
-
-"""This module sets up the budget_tracker package."""
-QApplication.setApplicationName("Budget Tracker")
-__version__ = QApplication.setApplicationVersion('1.1.0')
